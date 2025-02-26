9de9b3b88d28b575c58a9e6d916a2f4bd3eef6e8
Felipe Marques <114961292+felipmarques@users.noreply.github.com>
2025-02-26 10:59:55 -0300

    Update login.js

--- api/login.js
+++ api/login.js
@@ -29,26 +29,10 @@ export default async function handler(req, res) {
         return res.status(200).json({
           message: "Login bem-sucedido",
           usuario: resultUsuario.rows[0],
-          tipo: "usuario",
         });
       }
 
-      // Consulta na tabela usuario_promotor
-      const queryPromotor = `
-        SELECT * FROM usuario_promotor 
-        WHERE (cnpj = $1 OR telefone = $1) AND telefone = $2
-      `;
-      const resultPromotor = await pool.query(queryPromotor, [identificador, senha]);
-
-      if (resultPromotor.rowCount > 0) {
-        return res.status(200).json({
-          message: "Login bem-sucedido",
-          usuario: resultPromotor.rows[0],
-          tipo: "usuario_promotor",
-        });
-      }
-
-      // Se não encontrar em nenhuma tabela
+      // Se não encontrar na tabela usuario
       return res.status(401).json({ message: "Credenciais inválidas." });
     } catch (err) {
       console.error("Erro durante o login:", err);
