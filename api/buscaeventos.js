b55166586b21532cc7d9f85538e5d03b379c7c0b
Felipe Marques <114961292+felipmarques@users.noreply.github.com>
2025-04-08 18:42:44 -0300

    Update buscaeventos.js

--- api/buscaeventos.js
+++ api/buscaeventos.js
@@ -1,37 +1,39 @@
 const { Pool } = require('pg');
 
-// Conexão com o banco de dados Neon usando a string de conexão configurada no ambiente
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false // Necessário para a conexão com o Neon
+    rejectUnauthorized: false
   }
 });
 
 module.exports = async (req, res) => {
-  console.log('Requisição recebida:', req.method);
-
-  // Verificando se a requisição é do tipo GET (para buscar os eventos)
   if (req.method === 'GET') {
     try {
-      // Consulta para buscar todos os eventos da tabela 'eventos'
-      const query = 'SELECT nome, descricao, cep, endereco, link_ingresso, line_up, estado, tipo_evento FROM eventos';
+      const query = `
+        SELECT id, nome, descricao, cep, endereco, link_ingresso, line_up, estado, tipo_evento, data, local, imagem 
+        FROM eventos
+      `;
 
-      console.log('Executando query:', query);
-
-      // Executando a query para obter os dados dos eventos
       const result = await pool.query(query);
-      console.log('Resultado da query:', result.rows);
 
-      // Retorna a lista de eventos em formato JSON
-      res.status(200).json(result.rows);
+      // Convertemos o campo 'imagem' de bytea para base64
+      const eventos = result.rows.map(evento => {
+        return {
+          ...evento,
+          imagem: evento.imagem
+            ? `data:image/png;base64,${evento.imagem.toString('base64')}`
+            : null
+        };
+      });
+
+      res.status(200).json(eventos);
     } catch (err) {
-      // Tratamento de erro, se ocorrer algum problema na execução da query
-      console.error('Erro ao executar query:', err);
+      console.error('Erro ao buscar eventos:', err);
       res.status(500).send('Erro ao buscar eventos.');
     }
   } else {
-    // Caso o método da requisição não seja GET, retorna erro 405
     res.status(405).send('Método não permitido');
   }
 };
+
