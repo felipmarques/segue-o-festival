b79fc1d9f34cbd096bfb90f4b0956463cb6db06c
pwillig1 <89887186+pwillig1@users.noreply.github.com>
2025-04-28 15:41:01 -0300

    Criação do endpoint meusEventos.js
    Objetivo: Para listar eventos de promotor logado

---
+++ meusEventosPromotor.js
@@ -0,0 +1,41 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+module.exports = async (req, res) => {
+  if (req.method !== 'GET') {
+    return res.status(405).send('Método não permitido');
+  }
+
+  const { cnpj } = req.query; // Obtendo o CNPJ do usuário promotor da requisição
+
+  if (!cnpj) {
+    return res.status(400).send('Erro: CNPJ do usuário não fornecido.');
+  }
+
+  try {
+    const query = `
+      SELECT e.* 
+      FROM eventos e
+      JOIN usuario_promotor u ON e.id_promotor = u.cnpj
+      WHERE u.cnpj = $1
+      ORDER BY e.data DESC;
+    `;
+
+    const result = await pool.query(query, [cnpj]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).send('Nenhum evento encontrado para este usuário.');
+    }
+
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error('Erro ao buscar eventos:', error);
+    res.status(500).send('Erro ao recuperar eventos.');
+  }
+};
