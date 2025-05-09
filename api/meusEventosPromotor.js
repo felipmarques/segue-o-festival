088d72317612da35be8138297ebaf8c2355f8ee6
pwillig1 <89887186+pwillig1@users.noreply.github.com>
2025-05-09 15:16:57 -0300

    Update meusEventosPromotor.js

--- api/meusEventosPromotor.js
+++ api/meusEventosPromotor.js
@@ -1,11 +1,4 @@
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+const { Pool } = require('./db');
 
 module.exports = async (req, res) => {
   if (req.method !== 'GET') {
